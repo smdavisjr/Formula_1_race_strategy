e3df09dcc34d43d6195443873ba18174bc108b80
Stephen Davis <davisjr719@gmail.com>
2021-04-27 14:39:14 -0400

    Initial Commit

---
+++ F1-data_exploration.R
@@ -0,0 +1,50 @@
+#### Packages ####
+library(base)
+library(tidyverse)
+library(data.table)
+
+#### Import Data ####
+circuits <- as.data.table(fread('/Users/smdavisjr/Google Drive/Employment/Professional Development/R - Formula 1/Formula 1 Race Data/data/archive (1)/circuits.csv'))
+
+constructor_results <- as.data.table(fread('/Users/smdavisjr/Google Drive/Employment/Professional Development/R - Formula 1/Formula 1 Race Data/data/archive (1)/constructor_results.csv'))
+
+constructor_standings <- as.data.table(fread('/Users/smdavisjr/Google Drive/Employment/Professional Development/R - Formula 1/Formula 1 Race Data/data/archive (1)/constructor_standings.csv'))
+
+constructors <- as.data.table(fread('/Users/smdavisjr/Google Drive/Employment/Professional Development/R - Formula 1/Formula 1 Race Data/data/archive (1)/constructors.csv'))
+
+driver_standings <- as.data.table(fread('/Users/smdavisjr/Google Drive/Employment/Professional Development/R - Formula 1/Formula 1 Race Data/data/archive (1)/driver_standings.csv'))
+
+drivers <- as.data.table(fread('/Users/smdavisjr/Google Drive/Employment/Professional Development/R - Formula 1/Formula 1 Race Data/data/archive (1)/drivers.csv'))
+
+lap_times <- as.data.table(fread('/Users/smdavisjr/Google Drive/Employment/Professional Development/R - Formula 1/Formula 1 Race Data/data/archive (1)/lap_times.csv'))
+
+pit_stops <- as.data.table(fread('/Users/smdavisjr/Google Drive/Employment/Professional Development/R - Formula 1/Formula 1 Race Data/data/archive (1)/pit_stops.csv'))
+
+qualifying <- as.data.table(fread('/Users/smdavisjr/Google Drive/Employment/Professional Development/R - Formula 1/Formula 1 Race Data/data/archive (1)/qualifying.csv'))
+
+races <- as.data.table(fread('/Users/smdavisjr/Google Drive/Employment/Professional Development/R - Formula 1/Formula 1 Race Data/data/archive (1)/races.csv'))
+
+results <- as.data.table(fread('/Users/smdavisjr/Google Drive/Employment/Professional Development/R - Formula 1/Formula 1 Race Data/data/archive (1)/results.csv'))
+
+seasons <- as.data.table(fread('/Users/smdavisjr/Google Drive/Employment/Professional Development/R - Formula 1/Formula 1 Race Data/data/archive (1)/seasons.csv'))
+
+status <- as.data.table(fread('/Users/smdavisjr/Google Drive/Employment/Professional Development/R - Formula 1/Formula 1 Race Data/data/archive (1)/status.csv'))
+
+
+#### circuits ####
+
+
+
+#### constructor_results ####
+
+
+
+#### constructor_standings ####
+
+
+
+#### constructors ####
+
+
+
+#### 
