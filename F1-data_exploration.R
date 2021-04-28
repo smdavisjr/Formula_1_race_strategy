c26e0962532e38d94e942d36ac50332f9a6d4fc9
Stephen Davis <davisjr719@gmail.com>
2021-04-28 00:44:12 -0400

    Variable names to camelCase

--- F1-data_exploration.R
+++ F1-data_exploration.R
@@ -4,48 +4,42 @@ library(tidyverse)
 library(data.table)
 
 
-#### Import Data ####
+#### Import & str() Data ####
 circuits <- as.data.table(fread('./data/archive/circuits.csv'))
+str(circuits)
 
-constructor_results <- as.data.table(fread('./data/archive/constructor_results.csv'))
+constructorResults <- as.data.table(fread('./data/archive/constructor_results.csv'))
+str(constructorResults)
 
-constructor_standings <- as.data.table(fread('./data/archive/constructor_standings.csv'))
+constructorStandings <- as.data.table(fread('./data/archive/constructor_standings.csv'))
+str(constructorStandings)
 
 constructors <- as.data.table(fread('./data/archive/constructors.csv'))
+str(constructors)
 
-driver_standings <- as.data.table(fread('./data/archive/driver_standings.csv'))
+driverStandings <- as.data.table(fread('./data/archive/driver_standings.csv'))
+str(driverStandings)
 
 drivers <- as.data.table(fread('./data/archive/drivers.csv'))
+str(drivers)
 
-lap_times <- as.data.table(fread('./data/archive/lap_times.csv'))
+lapTimes <- as.data.table(fread('./data/archive/lap_times.csv'))
+str(lapTimes)
 
-pit_stops <- as.data.table(fread('./data/archive/pit_stops.csv'))
+pitStops <- as.data.table(fread('./data/archive/pit_stops.csv'))
+str(pitStops)
 
 qualifying <- as.data.table(fread('./data/archive/qualifying.csv'))
+str(qualifying)
 
 races <- as.data.table(fread('./data/archive/races.csv'))
+str(races)
 
 results <- as.data.table(fread('./data/archive/results.csv'))
+str(results)
 
 seasons <- as.data.table(fread('./data/archive/seasons.csv'))
+str(seasons)
 
 status <- as.data.table(fread('./data/archive/status.csv'))
-
-
-#### circuits ####
-
-
-
-#### constructor_results ####
-
-
-
-#### constructor_standings ####
-
-
-
-#### constructors ####
-
-
-
-#### 
+str(status)
